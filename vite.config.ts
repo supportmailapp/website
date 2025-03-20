29d8beeefa50114c3f1ed43048efc710c4d6f449
LukeZ <117578407+The-LukeZ@users.noreply.github.com>
2025-03-20 08:34:02 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import { paraglideVitePlugin } from '@inlang/paraglide-js';
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()],
+  plugins: [
+    tailwindcss(),
+    sveltekit(),
+    paraglideVitePlugin({
+      project: './project.inlang',
+      outdir: './src/lib/paraglide',
+      strategy: ["preferredLanguage", "localStorage", "cookie"],
+		}),
+  ],
 
   server: {
     port: 6060,
