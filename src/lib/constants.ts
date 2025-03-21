337bbde235beb8c5129844eac5cd2e8702763cdc
The-LukeZ <luke.hent3005@gmail.com>
2025-03-21 21:14:01 +0100

    feat(constants): add botAuth URL generation function with dynamic environment variables

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,17 @@
 // Constants (Public)
+
+import { env } from "$env/dynamic/public";
+
+export const urls = {
+  botAuth: function (clientId: string, guildId: string | null = null): string {
+    const params = new URLSearchParams({
+      client_id: env.PUBLIC_ClientId,
+      permissions: env.PUBLIC_botPermissions,
+      scope: "bot applications.commands",
+      response_type: "code",
+      redirect_uri: env.PUBLIC_discordRedirectUri,
+    });
+    if (guildId) params.append("guild_id", guildId);
+    return "https://discord.com/oauth2/authorize?" + params.toString();
+  },
+};
