f15128447cb64e69c864511e94d2ea93c0dc393d
The-LukeZ <luke.hent3005@gmail.com>
2025-03-22 22:46:10 +0100

    feat(coming-soon): replace old coming soon page with new layout
    now every page that hasn't been found redirects to a "coming soon" page

---
+++ src/routes/[slug]/+page.svelte
@@ -0,0 +1,3 @@
+<div class="my-20 flex-grow text-center italic">
+  <h1>coming soon...</h1>
+</div>
--- src/routes/coming-soon/+page.svelte
+++
@@ -1,3 +0,0 @@
-<div class="absolute bottom-0 flex h-full w-full items-center justify-center">
-  <span class="italic">coming soon...</span>
-</div>
