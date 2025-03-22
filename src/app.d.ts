2cb966db27063f331352bd4e6900f41597775fdf
The-LukeZ <luke.hent3005@gmail.com>
2025-03-23 00:11:25 +0100

    feat(types): extend PageData interface to include stats for guilds, users, and tickets

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,7 +2,13 @@ declare global {
   namespace App {
     // interface Error {}
     // interface Locals {}
-    // interface PageData {}
+    interface PageData {
+      stats: {
+        guilds: number;
+        users: number;
+        tickets: number;
+      }
+    }
     // interface PageState {}
     // interface Platform {}
   }
