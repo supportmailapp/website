69756ccdbd15470abd3450359eb352c3ed7f89bd
The-LukeZ <luke.hent3005@gmail.com>
2025-03-22 22:46:25 +0100

    fix(redirect): remove unnecessary environment variable from botAuth redirect

--- src/routes/add/+page.ts
+++ src/routes/add/+page.ts
@@ -1,7 +1,6 @@
-import { env } from "$env/dynamic/public";
 import { urls } from "$lib/constants.js";
 import { redirect } from "@sveltejs/kit";
 
 export const load = async function () {
-  return redirect(302, urls.botAuth(env.PUBLIC_ClientId));
+  return redirect(302, urls.botAuth());
 };
