0b18d5f16bdc665e5abb02feea11fc7c00e489fd
The-LukeZ <luke.hent3005@gmail.com>
2025-03-23 00:59:17 +0100

    fix(adapter): clean up Cloudflare adapter configuration options

--- svelte.config.js
+++ svelte.config.js
@@ -7,12 +7,10 @@ const config = {
   preprocess: vitePreprocess(),
   kit: {
     adapter: cloudflareAdapter({
-      // See below for an explanation of these options
       routes: {
         include: ["/*"],
         exclude: ["<all>"],
       },
-      fallback
     }),
   },
 };
