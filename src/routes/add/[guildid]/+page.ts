5c2ac29dcecfcec4cbb6bce0d0327fe15c24af21
The-LukeZ <luke.hent3005@gmail.com>
2025-03-22 22:47:00 +0100

    feat(redirect): rename /add/page.ts to +page.ts

--- src/routes/add/[guildid]/+page.ts
+++ src/routes/add/[guildid]/+page.ts
@@ -1,8 +1,7 @@
 import { page } from "$app/state";
-import { env } from "$env/dynamic/public";
 import { urls } from "$lib/constants.js";
 import { redirect } from "@sveltejs/kit";
 
 export const load = async function () {
-  return redirect(302, urls.botAuth(env.PUBLIC_ClientId, page.params.guildid));
+  return redirect(302, urls.botAuth(page.params.guildid));
 };
