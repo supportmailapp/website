f623b7b9d26cf1c505921618878d3979d67b64e2
The-LukeZ <luke.hent3005@gmail.com>
2025-03-23 00:43:31 +0100

    feat(adapter): add Cloudflare adapter and configure routing options

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,19 @@
-import adapter from "@sveltejs/adapter-node";
+import nodeAdpater from "@sveltejs/adapter-node";
+import cloudflareAdapter from "@sveltejs/adapter-cloudflare";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter(),
+    adapter: cloudflareAdapter({
+      // See below for an explanation of these options
+      routes: {
+        include: ["/*"],
+        exclude: ["<all>"],
+      },
+      fallback
+    }),
   },
 };
 
