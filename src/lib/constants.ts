c7f6407ea1ba12e9b9a404e276e4daef0b84298a
The-LukeZ <luke.hent3005@gmail.com>
2025-03-22 22:43:50 +0100

    refactor(constants): update botAuth function to prepare for dashboard integration and add legal links

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,15 +3,23 @@
 import { env } from "$env/dynamic/public";
 
 export const urls = {
-  botAuth: function (clientId: string, guildId: string | null = null): string {
+  // We will enable th redirect uri when the dashboard is ready
+  botAuth: function (guildId: string | null = null): string {
     const params = new URLSearchParams({
       client_id: env.PUBLIC_ClientId,
       permissions: env.PUBLIC_botPermissions,
       scope: "bot applications.commands",
-      response_type: "code",
-      redirect_uri: env.PUBLIC_discordRedirectUri,
+      // response_type: "code",
+      // redirect_uri: env.PUBLIC_discordRedirectUri,
     });
     if (guildId) params.append("guild_id", guildId);
     return "https://discord.com/oauth2/authorize?" + params.toString();
   },
+};
+
+export const legalLinks = {
+  base: env.PUBLIC_legalHost,
+  terms: env.PUBLIC_legalHost + "/terms",
+  privacy: env.PUBLIC_legalHost + "/privacy",
+  withdrawal: env.PUBLIC_legalHost + "/withdrawal",
 };
