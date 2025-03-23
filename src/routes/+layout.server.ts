cdb63fe16277b3e07a0e58d2f077da16e19c76a8
The-LukeZ <luke.hent3005@gmail.com>
2025-03-23 01:50:40 +0100

    feat(adapter): switch to Vercel adapter and update environment variable usage

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,10 +1,12 @@
+import { SUPPORTMAIL_API_KEY } from "$env/static/private";
+
 declare type StatsResponse = { guilds: number; users: number; tickets: number };
 
-export async function load({ platform }) {
+export async function load() {
   const result = await fetch("https://api.supportmail.dev/stats", {
     method: "GET",
     headers: {
-      Authorization: `Bearer ${platform?.env.SUPPORTMAIL_API_KEY}`,
+      Authorization: `Bearer ${SUPPORTMAIL_API_KEY}`,
     },
   })
     .then((res) => res.json() as Promise<StatsResponse>)
--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,13 @@
 import nodeAdpater from "@sveltejs/adapter-node";
-import cloudflareAdapter from "@sveltejs/adapter-cloudflare";
+import vercelAdapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: cloudflareAdapter({
-      routes: {
-        include: ["/*"],
-        exclude: ["<all>"],
-      },
+    adapter: vercelAdapter({
+      split: true,
     }),
   },
 };
