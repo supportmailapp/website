86e802635b3dba5c8e284d3ca4d3a1f40a7a533f
The-LukeZ <luke.hent3005@gmail.com>
2025-03-23 01:56:49 +0100

    feat(adapter): use conditional adapter based on NODE_ENV for production and development

--- svelte.config.js
+++ svelte.config.js
@@ -2,13 +2,18 @@ import nodeAdpater from "@sveltejs/adapter-node";
 import vercelAdapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
+const adapter =
+  process.env.NODE_ENV === "production"
+    ? vercelAdapter({
+        split: true,
+      })
+    : nodeAdpater();
+
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: vercelAdapter({
-      split: true,
-    }),
+    adapter: adapter,
   },
 };
 
