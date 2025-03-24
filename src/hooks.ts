a1abd30139388b306924ba6e565546ae328edfa6
The-LukeZ <luke.hent3005@gmail.com>
2025-03-25 00:04:59 +0100

    fix: add logging for URL rerouting in hooks.ts

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,5 +2,8 @@ import type { Reroute } from "@sveltejs/kit";
 import { deLocalizeUrl } from "$lib/paraglide/runtime";
 
 export const reroute: Reroute = (request) => {
-  return deLocalizeUrl(request.url).pathname;
+  console.log(`Reroute ${request.url.pathname}`);
+  const delocal = deLocalizeUrl(request.url).pathname;
+  console.log(`Delocalized to ${delocal}`);
+  return delocal;
 };
