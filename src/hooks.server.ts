d482e2811833bba070253e640c9dd593ad262821
The-LukeZ <luke.hent3005@gmail.com>
2025-03-25 00:04:54 +0100

    fix: remove unnecessary comment in hooks.server.ts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,6 @@
 import { paraglideMiddleware } from "$lib/paraglide/server";
 import type { Handle } from "@sveltejs/kit";
 
-// creating a handle to use the paraglide middleware
 const paraglideHandle: Handle = async ({ event, resolve }) =>
   paraglideMiddleware(event.request, ({ request: localizedRequest, locale }) => {
     event.request = localizedRequest;
