5e174baa8621f7b0f9a037f5f2ce497a01ee14c1
The-LukeZ <luke.hent3005@gmail.com>
2025-04-02 19:56:26 +0200

    feat: bump version to 1.2.3 and add browser check for logging in hooks

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,9 +1,10 @@
 import { type Reroute } from "@sveltejs/kit";
 import { deLocalizeUrl } from "$lib/paraglide/runtime";
+import { browser } from "$app/environment";
 
 export const reroute: Reroute = (request) => {
-  console.log(`Reroute ${request.url.pathname}`);
+  if (browser) console.log(`Reroute ${request.url.pathname}`);
   const delocal = deLocalizeUrl(request.url).pathname;
-  console.log(`Delocalized to ${delocal}`);
+  if (browser) console.log(`Delocalized to ${delocal}`);
   return delocal;
 };
