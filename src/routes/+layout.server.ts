6d09567648dc23f9340c4b72fe87ad5b51121cc6
The-LukeZ <luke.hent3005@gmail.com>
2025-04-14 01:36:37 +0200

    feat: enhance error handling in stats loading and add fallback response

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,16 +1,32 @@
 import { SUPPORTMAIL_API_KEY } from "$env/static/private";
 
+export const prerender = true;
+
 declare type StatsResponse = { guilds: number; users: number; tickets: number };
 
+const FALLBACK_STATS: StatsResponse = {
+  guilds: 550,
+  users: 180_000,
+  tickets: 2_000,
+};
+
 export async function load() {
+  console.log("Loading stats...", SUPPORTMAIL_API_KEY);
   const result = await fetch("https://api.supportmail.dev/stats", {
     method: "GET",
     headers: {
       Authorization: `Bearer ${SUPPORTMAIL_API_KEY}`,
     },
   })
-    .then((res) => res.json() as Promise<StatsResponse>)
-    .catch(() => new Object() as StatsResponse);
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Network response was not ok");
+      }
+      if (res.status === 404) return FALLBACK_STATS;
+
+      return res.json() as Promise<StatsResponse>;
+    })
+    .catch(() => FALLBACK_STATS);
 
   return {
     stats: result,
