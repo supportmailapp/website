93dd5ff6a69c55bb8511dbec6d5465ff6135aa9e
The-LukeZ <luke.hent3005@gmail.com>
2025-04-14 02:21:25 +0200

    refactor: remove legacy route handling and improve security check
    had no legacy routes anymore so Imma remove this

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,24 +3,6 @@ import { paraglideMiddleware } from "$lib/paraglide/server";
 import { redirect, type Handle } from "@sveltejs/kit";
 import { sequence } from "@sveltejs/kit/hooks";
 
-const legacyRoutes = new Map([
-  ["/legal", "https://legal.supportmail.dev"],
-  ["/invite", "https://help.supportmail.dev"],
-  ["/server", "https://help.supportmail.dev/"],
-  ["/pricing", "https://supportmail.dev/premium"],
-  ["/privacy", "https://legal.supportmail.dev/privacy"],
-  ["/terms", "https://legal.supportmail.dev/terms"],
-]);
-
-const legacyHandle: Handle = async ({ event, resolve }) => {
-  const legacyUrl = legacyRoutes.get(deLocalizeUrl(event.url).pathname);
-  if (legacyUrl) {
-    console.log(`Redirecting ${event.url.pathname} to ${legacyUrl}`);
-    redirect(301, legacyUrl);
-  }
-  return resolve(event);
-};
-
 const paraglideHandle: Handle = async ({ event, resolve }) =>
   paraglideMiddleware(event.request, ({ request: localizedRequest, locale }) => {
     event.request = localizedRequest;
@@ -35,14 +17,14 @@ const paraglideHandle: Handle = async ({ event, resolve }) =>
 const securityRegex = new RegExp(".*(\.env|config\/|config\.yml|config\.json|\.git|\.aws).*", "i");
 
 const securityRedirectHandle: Handle = async ({ event, resolve }) => {
-  if (event.url.pathname.includes("%20%20%20%20%22") || securityRegex.test(event.url.pathname)) {
+  if (event.url.pathname.includes("%20%20%20%20%22") || securityRegex.test(deLocalizeUrl(event.url).pathname)) {
     console.log(`Security check for "${event.url.pathname}" failed.`);
     redirect(303, "/newsletter");
   }
   return resolve(event);
 };
 
-export const handle = sequence(securityRedirectHandle, legacyHandle, paraglideHandle);
+export const handle = sequence(securityRedirectHandle, paraglideHandle);
 
 export async function handleError({ error, status, event, message }) {
   if (status !== 404) console.error(`Error ${status}: ${message}`, error);
