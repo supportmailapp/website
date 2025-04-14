70ccda5ea1bb4a67c1ccb26f6fadba01204d6533
The-LukeZ <luke.hent3005@gmail.com>
2025-04-14 02:43:07 +0200

    feat: implement Easter egg logic into /add route

---
+++ src/routes/add/[guildid]/+page.server.ts
@@ -0,0 +1,8 @@
+import { EasterEgg } from "$lib/constants.js";
+import { redirect } from "@sveltejs/kit";
+
+export async function load({ params }) {
+  if (params.guildid == "undefined") {
+    redirect(302, EasterEgg);
+  }
+}
