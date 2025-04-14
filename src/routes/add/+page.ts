a3f7c0161875a6abdab366d82313a38d8a45765a
The-LukeZ <luke.hent3005@gmail.com>
2025-04-14 02:48:14 +0200

    fix: restore prerender export in add routes for simplfication

--- src/routes/add/+page.ts
+++ src/routes/add/+page.ts
@@ -1,6 +1,8 @@
 import { urls } from "$lib/constants.js";
 import { redirect } from "@sveltejs/kit";
 
+export const prerender = false;
+
 export const load = async function () {
   return redirect(302, urls.botAuth());
 };
--- src/routes/add/[guildid]/+page.ts
+++ src/routes/add/[guildid]/+page.ts
@@ -2,6 +2,8 @@ import { page } from "$app/state";
 import { urls } from "$lib/constants.js";
 import { redirect } from "@sveltejs/kit";
 
+export const prerender = false;
+
 export const load = async function () {
   return redirect(302, urls.botAuth(page.params.guildid));
 };
