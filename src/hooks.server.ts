6afa6d034048efed885cdbb912e9e062d950cf71
The-LukeZ <luke.hent3005@gmail.com>
2025-04-14 02:50:09 +0200

    fix: use localizeHref for redirect in security check

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,4 +1,4 @@
-import { deLocalizeUrl } from "$lib/paraglide/runtime";
+import { deLocalizeUrl, localizeHref } from "$lib/paraglide/runtime";
 import { paraglideMiddleware } from "$lib/paraglide/server";
 import { redirect, type Handle } from "@sveltejs/kit";
 import { sequence } from "@sveltejs/kit/hooks";
@@ -19,7 +19,7 @@ const securityRegex = new RegExp(".*(\.env|config\/|config\.yml|config\.json|\.g
 const securityRedirectHandle: Handle = async ({ event, resolve }) => {
   if (event.url.pathname.includes("%20%20%20%20%22") || securityRegex.test(deLocalizeUrl(event.url).pathname)) {
     console.log(`Security check for "${event.url.pathname}" failed. IP:`, event.getClientAddress());
-    redirect(303, "/add/undefined");
+    redirect(303, localizeHref("/add/undefined"));
   }
   return resolve(event);
 };
