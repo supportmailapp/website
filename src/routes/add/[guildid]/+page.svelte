418664176f15380494cb8d59fe76b14fb70bbda3
The-LukeZ <luke.hent3005@gmail.com>
2025-04-30 20:23:27 +0200

    feat(routes): implement redirect logic for guild page navigation

---
+++ src/routes/add/[guildid]/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { goto } from "$app/navigation";
+  import { page } from "$app/state";
+  import { urls } from "$lib/constants";
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    goto(urls.botAuth(page.params.guildid));
+  });
+</script>
+
+<svelte:head>
+  <meta http-equiv="refresh" content="0; url=http://example.com/" />
+</svelte:head>
--- src/routes/add/[guildid]/+page.ts
+++ src/routes/add/[guildid]/+page.ts
@@ -1,9 +1,1 @@
-import { page } from "$app/state";
-import { urls } from "$lib/constants.js";
-import { redirect } from "@sveltejs/kit";
-
 export const prerender = false;
-
-export async function load() {
-  return redirect(302, urls.botAuth(page.params.guildid));
-}
