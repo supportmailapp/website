595df5bd57f59de1e43ad8cf474c68edbe3a4d09
The-LukeZ <luke.hent3005@gmail.com>
2025-06-18 23:58:33 +0200

    feat(login): implement bot authentication and form handling in new server file

---
+++ src/routes/login/+server.ts
@@ -0,0 +1,29 @@
+import { PUBLIC_ClientId } from "$env/static/public";
+
+export const prerender = false; // Disable prerendering for this route
+
+function botAuth({ state }: { state?: string } = {}) {
+  const url = new URL("https://discord.com/api/oauth2/authorize");
+  const searchP = new URLSearchParams({
+    client_id: PUBLIC_ClientId,
+    scope: "identify guilds guilds.members.read",
+    response_type: "code",
+    prompt: "true",
+    redirect_uri: "https://api.supportmail.dev/discord/callback",
+  });
+
+  if (state) searchP.set("state", state);
+  return url.toString() + `?${searchP.toString()}`;
+}
+
+export async function POST({ cookies, request }) {
+  const formData = await request.formData();
+  const stayLoggedIn = formData.get("stayLoggedIn") === "on";
+  const state = crypto.randomUUID();
+  cookies.set("state", state, { path: "/", sameSite: "lax" });
+  cookies.set("keep-refresh-token", String(stayLoggedIn), { path: "/", sameSite: "lax" });
+  console.log("State set in cookie:", state);
+  return Response.json({
+    url: botAuth({ state }),
+  });
+}
