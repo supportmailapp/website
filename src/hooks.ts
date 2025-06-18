bf072c8a1d9cde310c8eb207b0a312d4079df9de
The-LukeZ <luke.hent3005@gmail.com>
2025-06-18 21:47:29 +0200

    refactor(hooks): remove console logging for browser and server in reroute function

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,12 +1,7 @@
 import { type Reroute } from "@sveltejs/kit";
 import { deLocalizeUrl } from "$lib/paraglide/runtime";
-import { browser } from "$app/environment";
 
 export const reroute: Reroute = (request) => {
-  if (browser) console.log(`[Browser] Reroute ${request.url.pathname}`);
-  else console.log(`[Server] Reroute ${request.url.pathname}`);
   const delocal = deLocalizeUrl(request.url).pathname;
-  if (browser) console.log(`[Browser] Delocalized to ${delocal}`);
-  else console.log(`[Server] Delocalized to ${delocal}`);
   return delocal;
 };
