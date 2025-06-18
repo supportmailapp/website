e4d0950980d0b8e21d903996c43ac964097456be
The-LukeZ <luke.hent3005@gmail.com>
2025-06-18 14:52:45 +0200

    refactor(hooks): remove unused security checks and imports for clarity

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,20 +1,6 @@
-import { EasterEgg } from "$lib/constants";
-import { deLocalizeUrl, localizeHref } from "$lib/paraglide/runtime";
 import { paraglideMiddleware } from "$lib/paraglide/server";
-import { redirect, type Handle } from "@sveltejs/kit";
+import { type Handle } from "@sveltejs/kit";
 import { sequence } from "@sveltejs/kit/hooks";
-
-// For the funny ones...
-const securityRegex = new RegExp(".*(\.env|config\/|config\.yml|config\.json|\.git|\.aws).*", "i");
-
-// Under construction; Removed for simplicity
-const securityRedirectHandle: Handle = async ({ event, resolve }) => {
-  if (event.url.pathname.includes("%20%20%20%20%22") || securityRegex.test(deLocalizeUrl(event.url).pathname)) {
-    console.log(`Security check for "${event.url.pathname}" failed. IP:`, event.getClientAddress());
-    return Response.json({ secret: EasterEgg }, { status: 200 });
-  }
-  return resolve(event);
-};
 
 const paraglideHandle: Handle = async ({ event, resolve }) =>
   paraglideMiddleware(event.request, function resolveLocalized({ request: localizedRequest, locale }) {
