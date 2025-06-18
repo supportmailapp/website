cf6aadb393339f947e74d2e451663dc7b0a4a276
The-LukeZ <luke.hent3005@gmail.com>
2025-06-18 21:20:22 +0200

    feat(login): update success page to use localized messages for login success

--- src/routes/login/success/+page.svelte
+++ src/routes/login/success/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { m } from "$lib/paraglide/messages";
+
   // import { onMount } from "svelte";
 
   // TODO: Find a way to close the window after a countdown (close doesn't work becauseof "Scripts may close only the windows that were opened by them")
@@ -19,14 +21,7 @@
   // });
 </script>
 
-<!-- <div class="flex flex-col items-center gap-4">
-  <h2 class="text-success text-2xl font-bold">Login Successful!</h2>
-  <p class="text-lg">You have successfully logged in to SupportMail.</p>
-  <p class="text-sm text-slate-200">You can now return to Discord and close this page.</p>
-</div> -->
-
-<div class="bg-success text-success-content flex w-full max-w-lg flex-col rounded-xl p-4">
-  <h1 class="text-2xl font-bold">Login Successful!</h1>
-  <p class="text-lg">You have successfully logged in to SupportMail.</p>
-  <p class="text-sm text-slate-800">You can now return to Discord and close this page.</p>
+<div class="bg-success text-success-content flex w-full max-w-lg flex-col gap-1 rounded-xl p-4">
+  <h1 class="text-2xl font-bold">{m["login.loginSuccess"]()}</h1>
+  <p class="text-sm text-slate-800">{m["login.loginSuccessDescription"]()}</p>
 </div>
