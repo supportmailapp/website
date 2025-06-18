01c39e1eb1e06fc34e5496a8cc3225712e52f55b
The-LukeZ <luke.hent3005@gmail.com>
2025-06-18 14:51:34 +0200

    fix(constants): rename botAuth to botAdd and clean up unused parameters

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,13 +4,11 @@ import * as env from "$env/static/public";
 
 export const urls = {
   // We will enable th redirect uri when the dashboard is ready
-  botAuth: function (guildId: string | null = null): string {
+  botAdd: function (guildId: string | null = null): string {
     const params = new URLSearchParams({
       client_id: env.PUBLIC_ClientId,
       permissions: env.PUBLIC_botPermissions,
       scope: "bot applications.commands",
-      // response_type: "code",
-      // redirect_uri: env.PUBLIC_discordRedirectUri,
     });
     if (guildId) params.append("guild_id", guildId);
     return "https://discord.com/oauth2/authorize?" + params.toString();
