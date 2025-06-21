e9374d957cb1cdb247e6e921f87f4dba4e42d0d7
The-LukeZ <luke.hent3005@gmail.com>
2025-06-21 23:05:44 +0200

    fix(login): enhance botAuth function to support environment-specific client IDs and redirect URIs

--- src/routes/login/+server.ts
+++ src/routes/login/+server.ts
@@ -1,27 +1,29 @@
-import { PUBLIC_ClientId } from "$env/static/public";
+import { PUBLIC_ClientId_Dev, PUBLIC_ClientId_Prod } from "$env/static/public";
 
-function botAuth({ state }: { state?: string } = {}) {
+function botAuth({ state, isDev }: { state: string; isDev: boolean }) {
   const url = new URL("https://discord.com/api/oauth2/authorize");
   const searchP = new URLSearchParams({
-    client_id: PUBLIC_ClientId,
+    client_id: isDev ? PUBLIC_ClientId_Dev : PUBLIC_ClientId_Prod,
     scope: "identify guilds guilds.members.read",
     response_type: "code",
     prompt: "true",
-    redirect_uri: "https://api.supportmail.dev/discord/callback",
+    redirect_uri: isDev ? "http://localhost:3000/discord/callback" : "https://api.supportmail.dev/discord/callback",
   });
 
   if (state) searchP.set("state", state);
   return url.toString() + `?${searchP.toString()}`;
 }
 
-export async function POST({ cookies, request }) {
+export async function POST({ cookies, request, url }) {
   const formData = await request.formData();
   const stayLoggedIn = formData.get("stayLoggedIn") === "on";
   const state = crypto.randomUUID();
-  cookies.set("state", state, { path: "/", sameSite: "lax", domain: ".supportmail.dev" });
-  cookies.set("keep-refresh-token", String(stayLoggedIn), { path: "/", sameSite: "lax", domain: ".supportmail.dev" });
+  const isDev = url.searchParams.get("dev") === "true";
+  const cookiesDomain = isDev ? "localhost" : ".supportmail.dev";
+  cookies.set("state", state, { path: "/", sameSite: "lax", domain: cookiesDomain });
+  cookies.set("keep-refresh-token", String(stayLoggedIn), { path: "/", sameSite: "lax", domain: cookiesDomain });
   console.log("State set in cookie:", state);
   return Response.json({
-    url: botAuth({ state }),
+    url: botAuth({ state, isDev }),
   });
 }
