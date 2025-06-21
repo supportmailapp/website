b33b9f85e3e4da9d0b84a8b81bc427c4c1986306
The-LukeZ <luke.hent3005@gmail.com>
2025-06-21 23:05:48 +0200

    fix(login): clean up success page by removing unused code and adding development mode display

--- src/routes/login/success/+page.svelte
+++ src/routes/login/success/+page.svelte
@@ -1,27 +1,16 @@
 <script lang="ts">
+  import { page } from "$app/state";
   import { m } from "$lib/paraglide/messages";
 
-  // import { onMount } from "svelte";
-
-  // TODO: Find a way to close the window after a countdown (close doesn't work becauseof "Scripts may close only the windows that were opened by them")
-
-  // let countdown = $state(5);
-
-  // onMount(() => {
-  //   const interval = setInterval(() => {
-  //     countdown--;
-  //     if (countdown <= 0) {
-  //       clearInterval(interval);
-  //       var customWindow = window.open("", "_blank", "");
-  //       customWindow?.close();
-  //     }
-  //   }, 1000);
-
-  //   return () => clearInterval(interval);
-  // });
+  let isDev = $derived(page.url.searchParams.get("dev") === "true");
 </script>
 
 <div class="bg-success text-success-content flex w-full max-w-lg flex-col gap-1 rounded-xl p-4">
   <h1 class="text-2xl font-bold">{m["login.loginSuccess"]()}</h1>
   <p class="text-sm text-slate-800">{m["login.loginSuccessDescription"]()}</p>
+  {#if isDev}
+    <p class="text-sm text-slate-800">
+      Development Mode
+    </p>
+  {/if}
 </div>
