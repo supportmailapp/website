7354fe71f911fccece122925dd63f9ddc5fd4e96
The-LukeZ <luke.hent3005@gmail.com>
2025-07-06 15:56:37 +0200

    feat: add mdsvex support and update svelte configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,19 @@
+import { mdsvex } from "mdsvex";
 import adapter from "@sveltejs/adapter-cloudflare";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
-
+  preprocess: [vitePreprocess(), mdsvex()],
   kit: {
     adapter: adapter({
       config: undefined,
       fallback: "plaintext",
-      routes: {
-        include: ["/*"],
-        exclude: ["<all>"],
-      },
-      platformProxy: {
-        environment: "dev",
-      },
-    }),
+      routes: { include: ["/*"], exclude: ["<all>"] },
+      platformProxy: { environment: "dev" }
+    })
   },
+  extensions: [".svelte", ".svx"]
 };
 
 export default config;
