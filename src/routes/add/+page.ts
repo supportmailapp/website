61023f315929c1f244f0c0a4e4f67aa5be939a15
The-LukeZ <luke.hent3005@gmail.com>
2025-07-06 16:29:59 +0200

    fix: remove prerendering exports from various routes for improved rendering control

--- src/routes/add/+page.ts
+++ src/routes/add/+page.ts
@@ -1,8 +1,6 @@
 import { urls } from "$lib/constants.js";
 import { redirect } from "@sveltejs/kit";
 
-export const prerender = false;
-
 export const load = async function () {
   return redirect(302, urls.botAdd());
 };
--- src/routes/add/[guildid]/+page.server.ts
+++ src/routes/add/[guildid]/+page.server.ts
@@ -1,5 +1,3 @@
-export const prerender = false;
-
 export async function load({ params }) {
   return {
     easterEgg: params.guildid === "undefined",
