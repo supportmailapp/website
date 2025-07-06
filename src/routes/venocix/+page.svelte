79c45e7db7be11b80146035a0b6ab8d0aa27b6cc
The-LukeZ <luke.hent3005@gmail.com>
2025-07-06 15:57:08 +0200

    feat: create initial layout for Venocix page with developer information

---
+++ src/routes/venocix/+page.svelte
@@ -0,0 +1,18 @@
+
+
+<div class="container mx-auto max-w-[1200px] p-3">
+  <div class="card bg-base-100 flex-row">
+    <div class="avatar">
+      <div class="w-24 rounded-xl">
+        <img src="https://img.daisyui.com/images/profile/demo/yellingwoman@192.webp" />
+      </div>
+    </div>
+    <div class="card-body">
+      <h2 class="card-title">LukeZ</h2>
+      <p>Developer of SupportMail</p>
+    </div>
+  </div>
+  <p><i>Page in development. Come back in a few days for updates!</i></p>
+</div>
+
+
