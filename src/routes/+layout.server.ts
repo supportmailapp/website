b9d7aaf67173f606b8b9058f929acb2126806ee2
The-LukeZ <luke.hent3005@gmail.com>
2025-07-06 15:57:13 +0200

    fix: update fallback stats values for improved accuracy

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,35 +3,36 @@ import { building } from "$app/environment";
 declare type StatsResponse = { guilds: number; users: number; tickets: number; fallback?: true };
 
 const FALLBACK_STATS: StatsResponse = {
-  guilds: 580,
-  users: 198_000,
-  tickets: 2_030,
+  guilds: 660,
+  users: 216_300,
+  tickets: 2_300,
   fallback: true,
 };
 
 export async function load({ platform }) {
-  const result = building
-    ? FALLBACK_STATS
-    : await fetch("https://api.supportmail.dev/stats/current", {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${platform?.env.SUPPORTMAIL_API_KEY}`,
-        },
-      })
-        .then(async (res) => {
-          if (!res.ok) {
-            console.error("Failed to fetch stats from SupportMail API", res);
-          }
-          if (res.status === 404) return FALLBACK_STATS;
-
-          const _data = await res.json<StatsResponse>();
-          console.log("Fetched stats from SupportMail API", _data);
-          return _data;
+  const result =
+    building && platform?.env.SUPPORTMAIL_API_KEY
+      ? FALLBACK_STATS
+      : await fetch("https://api.supportmail.dev/stats/current", {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${platform?.env.SUPPORTMAIL_API_KEY}`,
+          },
         })
-        .catch((err) => {
-          console.error("Unknown Error fetching stats from SupportMail API", err);
-          return FALLBACK_STATS;
-        });
+          .then(async (res) => {
+            if (!res.ok) {
+              console.error("Failed to fetch stats from SupportMail API", res);
+            }
+            if (res.status === 404) return FALLBACK_STATS;
+
+            const _data = await res.json<StatsResponse>();
+            console.log("Fetched stats from SupportMail API", _data);
+            return _data;
+          })
+          .catch((err) => {
+            console.error("Unknown Error fetching stats from SupportMail API", err);
+            return FALLBACK_STATS;
+          });
 
   return {
     stats: result,
