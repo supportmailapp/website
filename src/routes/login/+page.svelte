5894e25cbb0aa12c0e72f7fff496550f0413cb35
The-LukeZ <luke.hent3005@gmail.com>
2025-08-30 20:09:11 +0200

    refactor: update login functionality to support auto-login option and modify related UI elements

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,52 +1,25 @@
 <script lang="ts">
-  import { enhance } from "$app/forms";
-  import { goto } from "$app/navigation";
-  import { page } from "$app/state";
   import { m } from "$lib/paraglide/messages";
-  import { onMount } from "svelte";
 
-  let showLoading = $state(false);
-
-  // onMount(() => {
-  //   const nextPath = page.url.searchParams.get("next");
-  //   if (nextPath?.startsWith("/app/")) {
-  //     goto(nextPath, { replaceState: true });
-  //   }
-  // });
+  let autoLoginNextTime = $state(false);
 </script>
 
-<form
-  class="grid place-items-center gap-5"
-  action="?/login"
-  method="POST"
-  use:enhance={() => {
-    const nextHref = page.url.searchParams.get("next");
-    if (nextHref?.startsWith("/app/")) {
-      localStorage.setItem("urlAfterLogin", nextHref);
-    }
+<div class="grid place-items-center gap-5">
+  <button
+    type="submit"
+    class="btn btn-primary btn-soft w-full max-w-xs"
+    onclick={(e) => {
+      e.preventDefault();
+      open(`https://client-api.supportmail.dev/discord/login?autoLogin=${String(autoLoginNextTime)}`, "_self");
+    }}
+  >
+    <img src="/icons/discord-mark-white.svg" alt="Discord Logo" class="size-8" />
 
-    showLoading = true;
-
-    return async ({ update, result }) => {
-      console.log("Form submitted", result);
-      await update();
-      if (result.type === "success" && result.data) {
-        open(result.data.url as string, "_self");
-      } else {
-        console.error("Error during login");
-      }
-      showLoading = false;
-    };
-  }}
->
-  <button type="submit" class="btn btn-primary btn-soft w-full max-w-xs" disabled={showLoading}>
-    {#if showLoading}
-      <div class="loading-spinner loading size-8"></div>
-    {:else}
-      <img src="/icons/discord-mark-white.svg" alt="Discord Logo" class="size-8" />
-    {/if}
-
-    <span class="text-lg text-white">{showLoading ? "" : m["login.loginWithDiscord"]()}</span>
+    <span class="text-lg text-white">{m["login.loginWithDiscord"]()}</span>
   </button>
+  <label class="label">
+    <input type="checkbox" class="checkbox checkbox-sm checkbox-primary" bind:checked={autoLoginNextTime} />
+    <span class="label-text text-xs text-white">{m["login.autoLoginNextTime"]()}</span>
+  </label>
   <p class="text-xs text-white">{m["login.loginDescription"]()}</p>
-</form>
+</div>
