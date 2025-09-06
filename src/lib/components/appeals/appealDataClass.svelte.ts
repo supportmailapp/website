2310d4899937edd6122f24d2be61af9cea371090
The-LukeZ <luke.hent3005@gmail.com>
2025-09-06 21:50:28 +0200

    feat: implement AppealData class for managing appeal-related state

---
+++ src/lib/components/appeals/appealDataClass.svelte.ts
@@ -0,0 +1,27 @@
+import { SvelteSet } from "svelte/reactivity";
+
+export class AppealData {
+  mode = $state<"user" | "server" | null>(null);
+  serverName = $state<string>("");
+  serverId = $state<string>("");
+  approxTime = $state<string>("");
+  receivedErrorMsgs = new SvelteSet<string>();
+  featureUsed = $state<string>("");
+  /**
+   * What the user thinks the reason for the action was.
+   */
+  acknowledgement = $state<string>("");
+  whichRulesViolated = new SvelteSet<string>();
+  readTos = $state<boolean>(false);
+  readPP = $state<boolean>(false);
+  ensureItDoesntHappenAgain = $state<string>("");
+  whatToInvestigateIfError = $state<string>("");
+  theoriesOnWhyThisWasMistaken = $state<string>("");
+  whatWouldHelpToUnderstand = new SvelteSet<string>();
+  isCommitedToFollowingRules = $state<string | null>(null);
+  additionalInfo = $state<string>("");
+  providedInfoIsAccurate = $state<boolean>(false);
+  understandConsequences = $state<boolean>(false);
+
+  constructor() {}
+}
