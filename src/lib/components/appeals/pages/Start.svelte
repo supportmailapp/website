008b3f28985521cb4e19ea19f7d782427746e619
The-LukeZ <luke.hent3005@gmail.com>
2025-09-06 22:04:52 +0200

    feat: add support for conditional rendering and markdown content in Start component

--- src/lib/components/appeals/pages/Start.svelte
+++ src/lib/components/appeals/pages/Start.svelte
@@ -2,8 +2,10 @@
   import { SvelteSet } from "svelte/reactivity";
   import type { AppealData } from "../appealDataClass.svelte";
   import SelectGroup from "../SelectGroup.svelte";
+  import Input from "../Input.svelte";
+  import { markdownToHtml } from "$lib";
 
-  let { data }: { data: AppealData } = $props();
+  let { data, show = false }: { data: AppealData; show?: boolean } = $props();
 
   const modes = [
     {
@@ -17,18 +19,34 @@
   ];
 
   const values = new SvelteSet<string>();
+
+  const textContent = [
+    "# SupportMail Appeals",
+    "",
+    "Welcome to SupportMail Appeals. Here you can try to get your or your server's access to the bot back.",
+    "All submitted data is handled according to our [Terms of Service](https://legal.supportmail.dev/terms) and [Privacy Policy](https://legal.supportmail.dev/privacy).",
+  ].join("\n");
 </script>
 
-<SelectGroup
-  label="For what do you appeal?"
-  options={modes}
-  min={1}
-  max={1}
-  {values}
-  showError={false}
-  onValueChange={(vals) => {
-    data.mode = vals.includes("user") ? "user" : "server";
-  }}
-/>
+{#if show}
+  <div class="sm-prose p-4">
+    {@html markdownToHtml(textContent, true)}
+  </div>
 
-<p>{modes.find((mode) => mode.value === data.mode)?.label}</p>
+  <SelectGroup
+    label="For what do you appeal?"
+    options={modes}
+    min={1}
+    max={1}
+    {values}
+    showError={false}
+    onValueChange={(vals) => {
+      data.mode = vals.includes("user") ? "user" : "server";
+    }}
+  />
+
+  {#if data.mode === "server"}
+    <Input bind:value={data.serverName} label="Server Name" />
+    <Input bind:value={data.serverId} label="Server ID" />
+  {/if}
+{/if}
