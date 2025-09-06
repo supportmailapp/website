5d30b1aff31cf9d7014816eca54707dea4b0bdc0
The-LukeZ <luke.hent3005@gmail.com>
2025-09-06 23:08:34 +0200

    refactor: restructure layout in +page.svelte for improved readability and organization

--- src/routes/appeal/+page.svelte
+++ src/routes/appeal/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { AppealData } from "$lib/components/appeals/appealDataClass.svelte";
+  import BanDetails from "$lib/components/appeals/pages/BanDetails.svelte";
   import Start from "$lib/components/appeals/pages/Start.svelte";
 
   const appealData = new AppealData();
@@ -19,21 +20,24 @@
   }
 </script>
 
-<div class="sm-prose p-4">
-  <h1>SupportMail Appeals</h1>
-</div>
+<div class="flex h-full flex-col justify-start">
+  <div class="sm-prose p-4">
+    <h1>SupportMail Appeals</h1>
+  </div>
 
-<div class="mx-auto max-w-3xl">
-  <Start data={appealData} show={pageIndex === 0} bind:inputsComplete={pageCompletions[pageIndex]} />
-</div>
+  <div class="mx-auto max-w-3xl">
+    <Start data={appealData} show={pageIndex === 0} bind:inputsComplete={pageCompletions[0]} />
+    <BanDetails data={appealData} show={pageIndex === 1} bind:inputsComplete={pageCompletions[1]} />
+  </div>
 
-<div class="mt-4 flex justify-center gap-4 p-4">
-  <button class="btn btn-primary" onclick={previousPage} disabled={pageIndex === 0}>Back</button>
-  <button
-    class="btn btn-primary"
-    onclick={nextPage}
-    disabled={pageIndex === pageCount - 1 || !pageCompletions[pageIndex]}
-  >
-    Next
-  </button>
+  <div class="mt-auto flex justify-center gap-4 p-4">
+    <button class="btn btn-primary" onclick={previousPage} disabled={pageIndex === 0}>Back</button>
+    <button
+      class="btn btn-primary"
+      onclick={nextPage}
+      disabled={pageIndex === pageCount - 1 || !pageCompletions[pageIndex]}
+    >
+      Next
+    </button>
+  </div>
 </div>
