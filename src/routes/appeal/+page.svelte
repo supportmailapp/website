d5d9b381ad580d6136b874e765be251b9fc5bb9d
The-LukeZ <luke.hent3005@gmail.com>
2025-09-06 21:51:11 +0200

    feat: add initial page structure and navigation for appeal process

--- src/routes/appeal/+page.svelte
+++ src/routes/appeal/+page.svelte
@@ -1,1 +1,23 @@
-X
+<script lang="ts">
+  import { AppealData } from "$lib/components/appeals/appealDataClass.svelte";
+  import Start from "$lib/components/appeals/pages/Start.svelte";
+
+  const appealData = new AppealData();
+  const pageCount = 8;
+  let pageIndex = $state<number>(0);
+
+  function nextPage() {
+    if (pageIndex < pageCount - 1) {
+      pageIndex += 1;
+    }
+  }
+  function previousPage() {
+    if (pageIndex > 0) {
+      pageIndex -= 1;
+    }
+  }
+</script>
+
+{#if pageIndex === 0}
+  <Start data={appealData} />
+{/if}
