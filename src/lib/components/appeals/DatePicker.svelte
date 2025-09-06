bf97f1abfb7a81b74800acca710f65dce325d46c
The-LukeZ <luke.hent3005@gmail.com>
2025-09-06 23:08:21 +0200

    feat: add DatePicker component for date selection in appeals

---
+++ src/lib/components/appeals/DatePicker.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import Fieldset from "./Fieldset.svelte";
+
+  type Props = {
+    label?: string;
+    description?: string;
+    value?: string;
+    onChange?: (value: string) => void;
+    min?: string;
+    max?: string;
+  };
+
+  let { label, description = "", value = $bindable(""), min, max, onChange }: Props = $props();
+</script>
+
+<Fieldset legend={label} labelAbove={description}>
+  <input
+    type="date"
+    class="input w-xs"
+    oninput={(e) => {
+      console.log("current value", e.currentTarget.value);
+      value = e.currentTarget.value;
+      onChange?.(e.currentTarget.value);
+    }}
+    {min}
+    {max}
+    bind:value
+  />
+</Fieldset>
